54959aafe44264f2226d89ec855dca5d4e51830b
Daeho Ro <lamanus@outlook.kr>
2024-08-20 12:00:12 +0900

    feature: add google analytics and remove fathom and posthog

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -14,46 +14,14 @@ import 'styles/notion.css'
 import 'styles/prism-theme.css'
 
 import type { AppProps } from 'next/app'
-import * as Fathom from 'fathom-client'
-import { useRouter } from 'next/router'
-import posthog from 'posthog-js'
+import { GoogleAnalytics } from 'nextjs-google-analytics'
 import * as React from 'react'
 
-import {
-  fathomConfig,
-  fathomId,
-  posthogConfig,
-  posthogId
-} from '@/lib/config'
-
 export default function App({ Component, pageProps }: AppProps) {
-  const router = useRouter()
-
-  React.useEffect(() => {
-    function onRouteChangeComplete() {
-      if (fathomId) {
-        Fathom.trackPageview()
-      }
-
-      if (posthogId) {
-        posthog.capture('$pageview')
-      }
-    }
-
-    if (fathomId) {
-      Fathom.load(fathomId, fathomConfig)
-    }
-
-    if (posthogId) {
-      posthog.init(posthogId, posthogConfig)
-    }
-
-    router.events.on('routeChangeComplete', onRouteChangeComplete)
-
-    return () => {
-      router.events.off('routeChangeComplete', onRouteChangeComplete)
-    }
-  }, [router.events])
-
-  return <Component {...pageProps} />
+  return (
+    <>
+      <GoogleAnalytics trackPageViews />
+      <Component {...pageProps} />
+    </>
+  )
 }
